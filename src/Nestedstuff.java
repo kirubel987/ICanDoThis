b5587487b9eb0d43d6a26ffa7f6d53376873172a
kirubel987 <kirubel.awetahegn@stu.ocsb.ca>
2022-12-08 14:11:23 -0500

    nested

--- src/Nestedstuff.java
+++ src/Nestedstuff.java
@@ -1,4 +1,16 @@
 
 public class Nestedstuff {
+	public static void Run() {
 
+	public static void main(String[] args) {
+		for(int r = 0;r < 10;r++) {
+			for(int c = 0;c < 10;c++) {
+				System.out.print("nested");
+				
+				
+			}
+			System.out.println();
+			
+		}
+	}
 }
